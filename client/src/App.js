ac2d3a13a38368da0882b94cfdc1f74a2ced1f2b
bamjam101 <herejamil123@gmail.com>
2022-10-07 00:08:40 +0530

    Added routing between different pages of the website using react-router-dom package.

--- client/src/App.js
+++ client/src/App.js
@@ -2,20 +2,30 @@ import Topbar from "./components/topbar/Topbar";
 import Home from "./pages/home/Home";
 import Write from "./pages/write/Write";
 import Register from "./pages/register/Register";
-import Footer from "./components/footer/Footer";
 import Login from "./pages/login/Login";
 import Account from "./pages/account/Account";
+import Footer from "./components/footer/Footer";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
+  const user = true;
   return (
-    <div className="App">
-        <Topbar/>
+    <Router>
+      <div className="App">
+        <Topbar />
         <main className="main-container">
-          <Account/>
+          <Routes>
+            <Route exact path="/" element={<Home/>} />
+            {user? <Route path="/register" element={<Home/>} /> : <Route path="/register" element={<Register/>} />}
+            {user? <Route path="/login" element={<Home/>} /> : <Route path="/login" element={<Login/>} />}
+            {user? <Route path="/write" element={<Write/>} /> : <Route path="/write" element={<Register/>} />}
+            {user? <Route path="/account" element={<Account/>} /> : <Route path="/account" element={<Register/>} />}
+          </Routes>
         </main>
-        <Footer/>
-    </div>
+        <Footer />
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
--- client/src/components/topbar/Topbar.js
+++ client/src/components/topbar/Topbar.js
@@ -1,6 +1,8 @@
 import "./Topbar.css";
+import { Link } from "react-router-dom";
 
 export default function Topbar() {
+  const user = true;
   return (
     <div className="Topbar">
       <header>
@@ -9,15 +11,27 @@ export default function Topbar() {
       </header>
       <nav>
         <ul>
-          <a href="#">
-            <li>Home</li>
-          </a>
-          <a href="#">
-            <li>About</li>
-          </a>
-          <a href="#">
-            <li>Contact</li>
-          </a>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>{user ? <Link to="/write">New Blog</Link> : ""}</li>
+          <li>{user ? <Link to="/account">Account</Link> : ""}</li>
+        </ul>
+      </nav>
+      <nav>
+        <ul>
+          {user ? (
+            ""
+          ) : (
+            <>
+              <li>
+                <Link to="/register">Register</Link>
+              </li>
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+            </>
+          )}
         </ul>
       </nav>
     </div>
