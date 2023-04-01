b04a5a2da6b30ec8f376979a13bb3f0841862078
bamjam101 <herejamil123@gmail.com>
2023-04-01 20:01:44 +0530

    Restored to previous state.

--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import axios from "axios";
 import "./Register.css";
-import { Link } from "react-router-dom";
 
 export default function Register() {
   const [username, setUsername] = useState("");
